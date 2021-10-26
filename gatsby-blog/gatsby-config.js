2c747a0a8fa7aacb23d8fbd088647ee6962322c2
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-10-26 11:52:02 +0500

    test

--- gatsby-blog/gatsby-config.js
+++ gatsby-blog/gatsby-config.js
@@ -1,30 +1,8 @@
-'use strict'
-
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-})
-
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Blog',
-    description: 'Gatsby blog with Strapi',
-    author: {
-      name: 'Muhammad Ahmad',
-      url: 'https://mahmad.netlify.app/'
-    }
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-source-strapi',
-      options: {
-        apiURL: process.env.API_URL || 'http://localhost:1337',
-        collectionTypes: [
-          // List of the Content Types you want to be able to request from Gatsby.
-          'article',
-          'category'
-        ],
-        queryLimit: 1000
-      }
+    siteMetadata: {
+        siteUrl: `https://www.yourdomain.tld`,
     },
-  ]
+    plugins: [
+
+    ]
 }
