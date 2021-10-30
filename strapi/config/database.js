dfd00ba198cbc60ff7317b460b08ae4a273d524b
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-10-30 18:58:36 +0500

    commit

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
+        client: 'sqlite',
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
@@ -13,3 +13,30 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+// const { parse } = require("pg-connection-string");
+
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+//   return {
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "bookshelf",
+//         settings: {
+//           client: "postgres",
+//           host,
+//           port,
+//           database,
+//           username: user,
+//           password,
+//           ssl: { rejectUnauthorized: false }
+//         },
+//         options: {
+//         },
+//       },
+//     },
+//   };
+// };
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '894d81af-ada1-4d59-94b5-15df8c013fb4'
+};
