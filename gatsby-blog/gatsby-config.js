6727b7e1dc2c08108adbcf96fc1a838d9e2d47d3
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-10-30 21:29:32 +0500

    modified for postgres

--- gatsby-blog/gatsby-config.js
+++ gatsby-blog/gatsby-config.js
@@ -1,8 +1,6 @@
 'use strict'
 
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-})
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
@@ -17,7 +15,9 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: process.env.API_URL || 'http://localhost:1337',
+        // apiURL: 'http://localhost:1337',
+        // apiURL: process.env.API_URL || 'http://localhost:1337',
+        apiURL: process.env.API_URL,
         collectionTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           'article',
