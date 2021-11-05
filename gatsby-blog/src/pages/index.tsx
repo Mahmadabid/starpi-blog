cadaed420cf4451f50612dff23bd7ddba66b4069
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-11-05 15:05:08 +0500

    added tailwind]

--- gatsby-blog/src/pages/index.tsx
+++ gatsby-blog/src/pages/index.tsx
@@ -1,6 +1,32 @@
+import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 
-const IndexPage = () => {
+const IndexPage = ({ dark, setdark }) => {
+
+  const result = useStaticQuery(graphql`{
+    allStrapiCategory {
+      edges {
+        node {
+          category
+          cate {
+            content
+            published_at
+            title
+            image {
+              url
+              name
+            }
+            id
+          }
+          strapiId
+        }
+      }
+    }
+  }`)
+
+  console.log(result);
+  console.log(dark);
+  
   return (
     <div>Hi</div>
   )
